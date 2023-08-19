285bcedffbf05cd9117494520eb1250fb82d40bb
Nandini Arora <nandini.arora.che22@itbhu.ac.in>
2023-08-19 18:39:06 +0530

    firebase done

---
+++ src/Firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA3Ch6-XeSCoy4uvSc2LEr88_zFTlMwUEM",
+  authDomain: "dev-f5a21.firebaseapp.com",
+  projectId: "dev-f5a21",
+  storageBucket: "dev-f5a21.appspot.com",
+  messagingSenderId: "898816768304",
+  appId: "1:898816768304:web:c3c257c376c854841b70f5",
+  measurementId: "G-FHVYFZMHL0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import { collection, addDoc } from "firebase/firestore";
+import {db} from '../../firebase';
 const Home = () => {
+    const addpswd = async (e) => {
+        e.preventDefault();  
+       
+        try {
+            const docRef = await addDoc(collection(db, "todos"), {
+              todo: todo,    
+            });
+            console.log("Document written with ID: ", docRef.id);
+          } catch (e) {
+            console.error("Error adding document: ", e);
+          }
+    }
   return (
+
     <div>
-        <input type="text" className=''/>
+        <h1 className='text-4xl'>Input field</h1>
+        <form action="">
+        <input type="text" className='outline '  placeholder="What do you have to do today?" onChange={(e)=>setTodo(e.target.value)}/>
+        </form>
+
     </div>
   )
 }
