8ccf6371825a956611bf8b0968900f41a0dc055f
Nandini Arora <nandini.arora.che22@itbhu.ac.in>
2023-08-19 20:01:58 +0530

    passwordssaved successfully in db

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,30 +8,34 @@ const Home = () => {
     const [password, setPassword] = useState("");
     const [pswds, setPswds] = useState([]);
     const fetchPost = async () => {
-        await getDocs(collection(db, "pswds"))
-            .then((querySnapshot)=>{               
-                const newData = querySnapshot.docs
-                    .map((doc) => ({...doc.data(), id:doc.id }));
-                setPswds(newData);                
-                console.log(pswds, newData);
-            })
-    }
-    
+        try {
+            const querySnapshot = await getDocs(collection(db, "pswds"));
+            const newData = querySnapshot.docs.map((doc) => ({
+                ...doc.data(),
+                id: doc.id
+            }));
+            setPswds(newData);
+            console.log(newData); // Log newData directly instead of pswds
+        } catch (error) {
+            console.error("Error fetching documents: ", error);
+        }
+    };
+
     useEffect(()=>{
         fetchPost();
     }, [])
     const addpswd = async (e) => {
-        e.preventDefault();  
+        e.preventDefault();
        
         try {
-            const docRef = await addDoc(collection(db, "pswd"), {
-              pswd: pswd,    
+            const docRef = await addDoc(collection(db, "pswds"), {
+                pswd: password, // Use the 'password' state variable here
             });
             console.log("Document written with ID: ", docRef.id);
-          } catch (e) {
-            console.error("Error adding document: ", e);
-          }
-    }
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
+    };
   return (
 
     <div>
--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -5,27 +5,46 @@ import React from 'react';
 
 const Home2 = () => {
   const [password, setPassword] = useState('');
-
   const handleSubmit = async (event) => {
     event.preventDefault();
     const length = 32;
-        const characters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*'
-        let password=''
-        for (let i=1; i<=length; i++){
-            const random= Math.floor(Math.random()*characters.length)
-            password+=characters[random];
-        }    
-    setPassword(password);
+    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*';
+    let newPassword = ''; // Change 'password' to 'newPassword'
+    for (let i = 1; i <= length; i++) {
+      const random = Math.floor(Math.random() * characters.length);
+      newPassword += characters[random];
+    }
+    setPassword(newPassword);
+  
+    try {
+      const docRef = await addDoc(collection(db, "password"), {
+        password: newPassword, // Use 'newPassword' instead of 'password'
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     const length = 32;
+//         const characters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*'
+//         let password=''
+//         for (let i=1; i<=length; i++){
+//             const random= Math.floor(Math.random()*characters.length)
+//             password+=characters[random];
+//         }    
+//     setPassword(password);
 
-  try {
-    const docRef = await addDoc(collection(db, "password"), {
-      password: password,    
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
-}
+//   try {
+//     const docRef = await addDoc(collection(db, "password"), {
+//       password: password,    
+//     });
+//     console.log("Document written with ID: ", docRef.id);
+//   } catch (e) {
+//     console.error("Error adding document: ", e);
+//   }
+// }
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
@@ -37,12 +56,12 @@ const Home2 = () => {
       <div className='flex flex-row items-center justify-center mb-6'>
       <form onSubmit={handleSubmit}>
         <label>
-          Which website is this password for?
-          <input type="text" placeholder="Website" className='text-purple-950 border-2 bg-white border-purple-800 rounded-lg text-base font-medium opacity-80 w-[500px] pl-2 py-2'/>
+          <b>Which website is this password for?</b> 
+          <input type="text" placeholder="Website" className='text-purple-950 border-2 mb-5 mt-2 bg-white border-purple-800 rounded-lg text-base font-medium opacity-80 w-[500px] pl-2 py-2'/>
         </label>
         <label>
-            What is your password?
-          <input type="password" placeholder="Enter your password" value={password} onChange={handlePasswordChange} className='text-purple-950 border-2 bg-white border-purple-800 rounded-lg text-base font-medium opacity-80 w-[500px] pl-2 py-2'/>
+           <b>What is your password?</b> 
+          <input type="password" placeholder="Enter your password" value={password} onChange={handlePasswordChange} className='text-purple-950 border-2 mt-2 bg-white border-purple-800 rounded-lg text-base font-medium opacity-80 w-[500px] pl-2 py-2'/>
         </label>
         <button type="submit" className='bg-purple-800 mt-5 text-white font-regular text-as py-2 px-8 border-2 hover:border-2 hover:border-purple-700 font-bold border-purple-400 rounded-full hover:bg-white hover:text-purple-800'>Submit</button>
       </form>
